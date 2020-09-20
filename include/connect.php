2a4567ab7ac2a8de16fc2fca59efa6b90089804e
Nhi123456 <71588632+Nhi123456@users.noreply.github.com>
2020-09-20 18:08:53 +0700

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$link=mysqli_connect("127.0.0.1:50333","azure","6#vWHD_$","dienthoai") or die("Cannot connect to the localhost");
+	$link=mysqli_connect("127.0.0.1:54560","Id=azure","6#vWHD_$","dienthoai") or die("Cannot connect to the localhost");
 	$link -> set_charset("utf8");
 ?>
